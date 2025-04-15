2191df29a31e360f437c843824a21753073bac77
chairina <chairina1109@gmail.com>
2025-04-15 19:53:52 +0700

    chore: add Dockerfile

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def calculate():
     return jsonify(operation=op, result=result)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
